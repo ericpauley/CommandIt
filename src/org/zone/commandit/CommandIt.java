5738790c62b9818a8720b9a825fd2617318d7dec
zonedabone <zonedabone@gmail.com>
2013-01-26 05:31:46 -0500

    Added some method outlines.

--- src/org/zone/commandit/CommandIt.java
+++ src/org/zone/commandit/CommandIt.java
@@ -1,8 +1,24 @@
 package org.zone.commandit;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 public class CommandIt extends JavaPlugin {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    @Override
+    public void onEnable() {
+        
+    }
     
 }
